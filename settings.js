62d459882f7febe64807a5bc3851ef363cfd95dc
Yahya17 Ch <70299820+yahya17YT@users.noreply.github.com>
2022-06-20 10:03:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,8 +12,8 @@
   }
 
   // Edit Sesuai Kemauanmu
-  global.autoReadGc = true // Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
-  global.autoReadAll = false // Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
+  global.autoReadGc = false // Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
+  global.autoReadAll = true // Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
   global.anticall = true // Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
   global.available = false // Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
   global.autoTyping = false // Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
@@ -23,18 +23,18 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = 'Yahya' // Ganti Jadi Namamu
+  global.owner = ['6285784715971'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6285784715971' // Owner Utama Istilahnya
+  global.botname = 'Yahya17Botz' // Ganti Jadi Nama Botmu
+  global.packname = 'Yahya17Botz' // Sticker Weem
+  global.author = 'Yahya17 Store' // Sticker Weem
+  global.webme = 'https://chat.whatsapp.com/HQ3Jb07y3cI8NQBCqZGYfH' // Ganti Sama Web Mu
+  global.linkgroup = 'https://chat.whatsapp.com/HQ3Jb07y3cI8NQBCqZGYfH' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://youtube.com/channel/UCYZ3s2hcv-3uYuR-BuO6wqg' // Ganti Sama Link Yt Mu
+  global.github = 'https://github.com/yahya17YT' // Ganti Sama Link Github Mu
+  global.instagram = 'https://instagram.com/yahya17_ch' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -67,7 +67,7 @@
   }
 
   // Kalo Mau Menyesuaikan Nama File Foto Nya
-  global.thumb = fs.readFileSync('./media/hinata.jpg')
+  global.thumb = fs.readFileSync('./media/yahya.png')
     
   // Ini Gausah Diubah
   let file = require.resolve(__filename)
